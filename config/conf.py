28142ab930b7f6004fd56b6cd7e89c9b1b4ad289
Kuzmitsky Alexey <lecha4444@mail.ru>
2023-12-10 21:39:20 +0300

    PORTAL_Fixer 2.0.3 GUI

--- config/conf.py
+++ config/conf.py
@@ -1,6 +1,6 @@
 name_program = 'PORTAL_Fixer'
-version_program = '2.0.2a'
-program_compilation_date = '2023.12.08'
+version_program = '2.0.3'
+program_compilation_date = '2023.12.10'
 
 program_directory_map = {
     'logs': None,
